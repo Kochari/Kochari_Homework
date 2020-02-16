9c6e71a5ee262bcea89a3f769725173ce6b6557b
Kochari <koceri.agalarov@gmail.com>
2020-02-16 17:47:55 +0400

    homewor

---
+++ src/main/java/hw01/HomeworkOne.java
@@ -0,0 +1,50 @@
+package hw01;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.*;
+
+public class HomeworkOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int randomNumber = (int) (10 * Math.random());
+
+        int[] playerNumber = new int[100];
+
+        System.out.println("Enter player name: ");
+        String name = sc.nextLine();
+        System.out.println("Let the game begin!");
+        int count = 0;
+
+        for (int infinitive = 0; ; infinitive++) {
+            System.out.println("Enter player number between 0 to 100: ");
+            playerNumber[infinitive] = sc.nextInt();
+            count++;
+            if (playerNumber[infinitive] != (int) playerNumber[infinitive]) {
+                System.out.println("It is not a number.Please, try again.");
+            }
+             else if(playerNumber[infinitive] < randomNumber) {
+                System.out.println("Your number is too small. Please, try again.");
+            } else if (playerNumber[infinitive] > randomNumber) {
+                System.out.println("Your number is too big. Please, try again.");
+            } else {
+                System.out.println("Congratulations, " + name);
+                break;
+            }
+        }
+        for (int i = 0; i < count; i++) {
+            for (int j = i + 1; j < count; j++) {
+                if (playerNumber[i] < playerNumber[j]) {
+                    playerNumber[i] = playerNumber[i] + playerNumber[j];
+                    playerNumber[j] = playerNumber[i] - playerNumber[j];
+                    playerNumber[i] = playerNumber[i] - playerNumber[j];
+
+                }
+            }
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println(playerNumber[i]);
+        }
+
+    }
+}
