2fec68f3567efa2dae0e9e543387c4c6de30edbc
Kochari <koceri.agalarov@gmail.com>
2020-03-30 19:58:33 +0400

    commit -m "hw6"

---
+++ src/main/java/hw08/Test/FamilyTest.java
@@ -0,0 +1,39 @@
+package hw08.Test;
+
+import hw08.Family;
+import hw08.Human;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class FamilyTest {
+    Family family;
+
+    Human connie = new Human("Connie", "Karleone", 1999);
+    @BeforeEach
+    void before() {
+        Human father = new Human("Vito ", "Karleone");
+        Human mother = new Human("Jane ", "Karleone");
+        Human fredo = new Human("Fredo", "Karleone", 1996);
+
+        family = new Family(father, mother);
+        family.addChild(fredo);
+    }
+
+    @Test
+    void countFamily() {
+        int expected = 3;
+        int actual = family.countFamily();
+        assertEquals(expected, actual);
+    }
+    @Test
+    void addChild() {
+        assertEquals(3, family.countFamily());
+        assertTrue(family.addChild(connie));
+        assertEquals(4, family.countFamily());
+    }
+
+}
